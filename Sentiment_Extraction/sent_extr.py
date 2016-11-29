339e66770c06eaaf643e9441cb700f4aa323a38e
albertomun <alberto.munisso@gmail.com>
2016-11-29 12:43:15 +0100

    Article ID (unmodified VADER)

--- Sentiment_Extraction/sent_extr.py
+++ Sentiment_Extraction/sent_extr.py
@@ -1,3 +1,5 @@
+## MODULES ##
+#
 import cPickle
 import pandas as pd
 from nltk.tokenize import sent_tokenize
@@ -7,16 +9,53 @@ import httplib2
 from googleapiclient import discovery
 from oauth2client.client import GoogleCredentials
 import numpy as np
-# select one article by using articles[1]['article']
 
 # Initialization
+#
 with open("wheat_articles.txt", "r") as fp:
-    articles = cPickle.load(fp)
+    wheat_articles = cPickle.load(fp)
+
+# Take a sample
+#    
+test_sample_size = 1000
     
 
 ## SENTIMENT EXTRACTION ##
 
 # Google NLP INITIALIZATION
+#
+def retrieve_sentiment(string):
+    credentials = GoogleCredentials.get_application_default()
+    service = discovery.build('language', 'v1beta1', credentials=credentials)
+    service_request = service.documents().analyzeSentiment(
+        body={
+            'document': {
+## MODULES ##
+#
+import cPickle
+import pandas as pd
+from nltk.tokenize import sent_tokenize
+from vaderSentiment.vaderSentiment import sentiment as vaderSentiment
+from random import choice
+import httplib2
+from googleapiclient import discovery
+from oauth2client.client import GoogleCredentials
+import numpy as np
+
+# Initialization
+#
+with open("wheat_articles.txt", "r") as fp:
+    wheat_articles = cPickle.load(fp)
+
+# Take a sample
+#    
+test_sample_size = 1000
+    
+
+## SENTIMENT EXTRACTION ##
+
+# Google NLP INITIALIZATION
+#
 def retrieve_sentiment(string):
     credentials = GoogleCredentials.get_application_default()
     service = discovery.build('language', 'v1beta1', credentials=credentials)
@@ -34,6 +73,7 @@ def retrieve_sentiment(string):
     return {'polarity': polarity, 'magnitude': magnitude}
 
 # VADER & GOOGLE NLP
+#
 def test_VADER_GoogleNLP(test):
     VADER = list()
     Google_NLP = list()
@@ -42,7 +82,7 @@ def test_VADER_GoogleNLP(test):
     test['article'] = test['article'].replace(',', '.')   # All the , are replaced with .
     highlights = ["wheat"]                                # Looks in each sentence for the word "wheat"
     sentencesWithHighlights = []                          # Stores all the sentences containing "wheat"
-    for sentence in sent_tokenize(test['article']):       # One sentence is contained between two . (or fist article's sentence)
+    for sentence in sent_tokenize(test['article']):       # One sentence is contained between two . (or first article's sentence)
        for highlight in highlights:
            if highlight in sentence:
               sentencesWithHighlights.append(sentence)
@@ -60,26 +100,35 @@ def test_VADER_GoogleNLP(test):
 		    VADER.append(vs)           
     df_sentiment = pd.DataFrame(VADER)                                                              # Change here Google_NLP or VADER
     df_sentiment['Date'] = test['date']                                                             # Gives article date
-    df = pd.concat([df_sentences, df_sentiment['compound'], df_sentiment['Date']], axis=1)          # Panda output, add sentences if possible
+    df_sentiment['article_id'] = test['article_id'] 
+    df = pd.concat([df_sentiment['article_id'],df_sentences, df_sentiment['compound'], df_sentiment['Date']], axis=1)          # Panda output, add sentences if possible
     return df
 
-
+    
 # Data Refinement
-test1 = articles                           # Insert here articles interval to be analyzed
+#
+test1 = wheat_articles[:test_sample_size]                        # Insert here articles interval to be analyzed
 test = []
 word = "wheat" 
+phrase = "wheat futures dodge bullet"
 for i in range(len(test1)):   
     if word in test1[i]['article']: 
-        test.append(test1[i])
+       if phrase not in test1[i]['article']: 
+          test.append(test1[i])
 
 
-# Sentiment Extraction		
+# Sentiment Extraction
+#		
 sentiment = pd.DataFrame()
 for i in range(len(test)):
     sentiment = sentiment.append([test_VADER_GoogleNLP(test[i])])
-    
+
+
+# Give df column names
+#
 sentiment['compound'].replace(r'\s+', np.nan, regex=True)
+sentiment.columns = ['Article_ID','Sentence', 'Compound', 'Date']
 
 ## OUTPUT ##
-
-sentiment.to_csv('df.csv', sep=';',index=False)
+#
+sentiment.to_csv('df2.csv', sep=';',index=False)
