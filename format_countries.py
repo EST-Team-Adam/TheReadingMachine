07636ec38a4606343444bacdccb75d7b20421282
marcogarieri <marcogarieri@gmail.com>
2016-12-23 23:24:04 +0100

    Final

--- format_countries.py
+++ format_countries.py
@@ -3,6 +3,7 @@ from thereadingmachine.geotagging import check_countries
 from thereadingmachine.geotagging import read_countries
 from thereadingmachine.process import read_jsonl
 import sys, re
+from json import dump
 from collections import defaultdict
 
 
@@ -13,7 +14,10 @@ from collections import defaultdict
 file_prefix = "data/amis_articles"
 version = '27_11_2016'
 input_file_name = '{0}_{1}_indexed.jsonl'.format(file_prefix, version)
-output_file_name = '{0}_{1}_geotagged.jsonl'.format(file_prefix, version)
+output_file_name = '{0}_{1}_geotagged_from_indexed.jsonl'.format(file_prefix, version)
+## For MK: if you need to run it after your processing run the two commands commented instead
+#input_file_name = '{0}_{1}_processed.jsonl'.format(file_prefix, version)
+#output_file_name = '{0}_{1}_geotagged_from_processed.jsonl'.format(file_prefix, version)
 test_sample_size = 1000
 # Getting file of list of countries 
 file_countries = "data/list_of_countries.csv"
@@ -29,9 +33,12 @@ d_countries = defaultdict(list)
 for article in articles:
     article.update({'geo_tag': check_countries(article['article'], d_countries)})
 
-test = 1
-
-
+# Save the processed file
+# --------------------------------------------------
+with open(output_file_name, 'w') as f:
+    for article in articles:
+        dump(article, f)
+        f.write('\n')
 
 
 
