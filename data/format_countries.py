736dee009e2b27287f180f1464aa6130b6428dbf
marcogarieri <marcogarieri@gmail.com>
2016-12-23 14:25:02 +0100

    Setting up

--- data/format_countries.py
+++ data/format_countries.py
@@ -1,38 +1,29 @@
-import re
+from thereadingmachine.geotagging import make_dict_countries
+from thereadingmachine.geotagging import check_countries
 import sys
-import os
 from collections import defaultdict
 
-d_countries = defaultdict()
+
+# Reading data
+# --------------------------------------------------
+
+# Initiate file names and parameters
+file_prefix = "data/amis_articles"
+version = '27_11_2016'
+input_file_name = '{0}_{1}_indexed.jsonl'.format(file_prefix, version)
+output_file_name = '{0}_{1}_geotagged.jsonl'.format(file_prefix, version)
+test_sample_size = 1000
+
+# Getting file of list of countries 
+
+# Read the data
+articles = read_jsonl(input_file_name)
 
 input_file_name = sys.argv[1]
 
-## Create dictionary
-def make_dict_countries(raw, d_rep, separ = ";"):
-    id_country, list_synon = raw.split(separ)
-    ## Get list of synonymous names
-    list_synon = list_synon.split(",")
-    ## Add as synonymous the country name itself
-    list_synon.append(id_country)
-    ## Remove empty synonymous
-    list_synon = filter(None, list_synon)
-    ## Remove if any duplicates
-    list_synon = list(set(list_synon))
-    ## Add to dictionary
-    [d_rep[id_country].append(x) for x in list_synon]
-
-
-## Return countries in article
-def check_countries(text, dict):
-    results = []
-    for k in dict.keys():
-        if any(words in text for words in dict[k]):
-            results.append(k)
-    results = list(set(results))
-    return results
-              
-## Formatting input file
 list_countries = []
+d_countries = defaultdict()
+
 with open(input_file_name, 'rU') as f:
     for line in f:
         ## Remove all parenthesis and their content
