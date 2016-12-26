77efbee9a493265f9a6561e46ef8f0fc0d0df81d
mkao006 <mkao006@gmail.com>
2016-12-26 14:59:37 +0800

    added parallel to the sentiment extraction

--- google_nlp_api_sentiment.py
+++ google_nlp_api_sentiment.py
@@ -1,6 +1,15 @@
+import os
 import json
+import multiprocessing
+from joblib import Parallel, delayed
 from googleapiclient import discovery
 from oauth2client.client import GoogleCredentials
+
+# Initialisation
+file_prefix = "data/amis_articles"
+version = '27_11_2016'
+input_file_name = '{0}_{1}_processed.jsonl'.format(file_prefix, version)
+output_file_name = '{0}_{1}_sentiment.jsonl'.format(file_prefix, version)
 
 # Define the function to retrieve sentiments from Google NLP API.
 
@@ -30,25 +39,28 @@ def retrieve_sentiment(string):
         magnitude = 0
     return {'polarity': polarity, 'magnitude': magnitude}
 
-# Initialisation
-file_prefix = "data/amis_articles"
-version = '27_11_2016'
-input_file_name = '{0}_{1}_processed.jsonl'.format(file_prefix, version)
-output_file_name = '{0}_{1}_sentiment.jsonl'.format(file_prefix, version)
 
-# Append sentiments to the articles
-#
+def process_sentiment(line, output_file_folder='data/sentiment/'):
+    article = json.loads(line)
+    sentiment = retrieve_sentiment(article['article'])
+    sentiment.update({'id': article['id']})
+    print(sentiment)
+    output_file = output_file_folder + article['id'] + ".json"
+    with open(output_file, 'w') as f:
+        json.dump(sentiment, f)
+        f.write('\n')
+
 # NOTE (Michael): It seems that the computational cost of the
 #                 sentiment using the Google NLP API is quite high and
 #                 may take a great amount of time to compute the
 #                 sentiments of all articles. Further, there may be
 #                 limits to the request or charges.
 
-with open(input_file_name) as fi, open(output_file_name, 'w') as fo:
-    for line in fi:
-        article = json.loads(line)
-        sentiment = retrieve_sentiment(article['article'])
-        sentiment.update({'id': article['id']})
-        print(sentiment)
-        json.dump(sentiment, fo)
-        fo.write('\n')
+output_folder = 'data/sentiment/'
+if not os.path.exists(output_folder):
+    os.makedirs(output_folder)
+
+num_cores = multiprocessing.cpu_count()
+with open(input_file_name) as fi:
+    Parallel(n_jobs=num_cores)(delayed(process_sentiment)(line)
+                               for line in fi)
