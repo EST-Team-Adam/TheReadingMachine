eb3f6dbc5e88997f2baa07831c84239bca02b521
mkao006 <mkao006@gmail.com>
2016-12-26 14:41:32 +0800

    processed sentiment line by line

--- google_nlp_api_sentiment.py
+++ google_nlp_api_sentiment.py
@@ -1,7 +1,6 @@
-import httplib2
+import json
 from googleapiclient import discovery
 from oauth2client.client import GoogleCredentials
-from thereadingmachine.process import read_jsonl
 
 # Define the function to retrieve sentiments from Google NLP API.
 
@@ -10,30 +9,32 @@ def retrieve_sentiment(string):
     ''' Use the google nlp api to calculate the sentiments.
     '''
 
-    credentials = GoogleCredentials.get_application_default()
-    service = discovery.build('language', 'v1beta1', credentials=credentials)
-    service_request = service.documents().analyzeSentiment(
-        body={
-            'document': {
-                'type': 'PLAIN_TEXT',
-                'content': string,
+    try:
+        credentials = GoogleCredentials.get_application_default()
+        service = discovery.build(
+            'language', 'v1beta1', credentials=credentials)
+        service_request = service.documents().analyzeSentiment(
+            body={
+                'document': {
+                    'type': 'PLAIN_TEXT',
+                    'content': string,
+                }
             }
-        }
-    )
-    response = service_request.execute()
+        )
+        response = service_request.execute()
 
-    polarity = response['documentSentiment']['polarity']
-    magnitude = response['documentSentiment']['magnitude']
+        polarity = response['documentSentiment']['polarity']
+        magnitude = response['documentSentiment']['magnitude']
+    except:
+        polarity = 0
+        magnitude = 0
     return {'polarity': polarity, 'magnitude': magnitude}
 
 # Initialisation
 file_prefix = "data/amis_articles"
-version = '27_07_2016'
+version = '27_11_2016'
 input_file_name = '{0}_{1}_processed.jsonl'.format(file_prefix, version)
-
-# Read the data
-articles = read_jsonl(input_file_name)
-
+output_file_name = '{0}_{1}_sentiment.jsonl'.format(file_prefix, version)
 
 # Append sentiments to the articles
 #
@@ -42,5 +43,12 @@ articles = read_jsonl(input_file_name)
 #                 may take a great amount of time to compute the
 #                 sentiments of all articles. Further, there may be
 #                 limits to the request or charges.
-for article in articles:
-    article.update(retrieve_sentiment(article['article']))
+
+with open(input_file_name) as fi, open(output_file_name, 'w') as fo:
+    for line in fi:
+        article = json.loads(line)
+        sentiment = retrieve_sentiment(article['article'])
+        sentiment.update({'id': article['id']})
+        print(sentiment)
+        json.dump(sentiment, fo)
+        fo.write('\n')
