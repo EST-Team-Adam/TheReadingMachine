e665de2395786403ef5e69c21e0c35e74a3e6c63
albertomun <alberto.munisso@gmail.com>
2017-01-06 15:04:26 +0100

    Update kalman_filter.R

--- Sentiment_Analysis/kalman_filter.R
+++ Sentiment_Analysis/kalman_filter.R
@@ -13,15 +13,22 @@ P0 <- matrix(100)
 
 ## PARAMETERS ESTIMATION ##
 fit.fkf <- optim(c(HHt = var(y, na.rm = TRUE) * .5,
-GGt = var(y, na.rm = TRUE) * .5),
-fn = function(par, ...)
--fkf(HHt = matrix(par[1]), GGt = matrix(par[2]), ...)$logLik,
-yt = rbind(y), a0 = a0, P0 = P0, dt = dt, ct = ct,
-Zt = Zt, Tt = Tt, check.input = FALSE)
+                 GGt = var(y, na.rm = TRUE) * .5),
+                 fn = function(par, ...)
+                 -fkf(HHt = matrix(par[1]), 
+                      GGt = matrix(par[2]), ...)$logLik,
+                 yt = rbind(y), a0 = a0, P0 = P0, dt = dt, ct = ct,
+                 Zt = Zt, Tt = Tt, check.input = FALSE)
 
 ## FILTER ##
-fkf.obj <- fkf(a0, P0, dt, ct, Tt, Zt, HHt = matrix(fit.fkf$par[1]),
-GGt = matrix(fit.fkf$par[2]), yt = rbind(y))
+fkf.obj <- fkf(a0, 
+               P0, 
+               dt, 
+               ct, 
+               Tt, 
+               Zt, 
+               HHt = matrix(fit.fkf$par[1]),
+               GGt = matrix(fit.fkf$par[2]), yt = rbind(y))
 
 ## COMPARISONS
 fit.stats <- StructTS(y, type = "level")
