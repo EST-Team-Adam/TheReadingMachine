90644cbec472f0f8d0bdf8996e5fec6e067c0a7b
albertomun <alberto.munisso@gmail.com>
2017-02-27 16:04:41 +0100

    Sentence_Topic_POC v1.1
    Added an article cleaner (removes a few characters and a few french sentences)
    Added Sentiment extraction (VADER and Google NLP). Google NLP reaches the upper limit of requests.
    Keyword extractor has been inserted into thereadingmachine from the main script.

--- Sentences_Topic/Sentence_Topic_POC.py
+++ Sentences_Topic/Sentence_Topic_POC.py
@@ -1,6 +1,8 @@
 from thereadingmachine.process import read_jsonl
 from thereadingmachine.SBD import SBD
-from thereadingmachine.sentence_tagger import Topic_Extractor  # to be eliminated from here
+from thereadingmachine.article_keywords import article_keywords
+from thereadingmachine.sentiment_VADER import VADER
+from thereadingmachine.sentiment_GoogleNLP import GoogleNLP
 from json import dump
 
 
@@ -10,45 +12,25 @@ version = '27_11_2016'
 input_file_name = '{0}_{1}_indexed.jsonl'.format(file_prefix, version)
 output_file_name = '{0}_{1}_sentences_from_indexed.jsonl'.format(file_prefix, version)
 
-# Functions
-def main(sentence):
-    topic_extractor = Topic_Extractor(sentence)
-    result = topic_extractor.extract()
-    results.append(result)
-    return results
-    
-
-def sentence_by_sentence(article_sentences):
-    for sentence in article_sentences:
-        if __name__ == '__main__':
-            result_article = main(sentence)
-            results_article.append(result_article)
-    return results_article
-
-
-
-# Introduction (for paper purposes only)
-#sentence = "Wheat market is showing a positive trend: trained by last year good American production, wheat price arose more than prices of rice and soybeans."
-#results = list()
-#results = main(sentence) 
 
 # Read the data
 articles = read_jsonl(input_file_name)
 
-# Test Article
-tests = articles
 
-# Extract Topics
+# Test Article
+tests = articles#[0:100]#[98345:98347]
+
+
+# Extract Topics and Sentiment
 for test in tests:
   article_sentences = SBD(test)
-  result_article = list()
-  results_article = list()
-  results = list()
-  results_article = sentence_by_sentence(article_sentences)
+  results_article = article_keywords(article_sentences)
+  sentiment_VADER = VADER(article_sentences)
+  #sentiment_Google = GoogleNLP(article_sentences)
   dict = {}
   for i in range(len(article_sentences)):
-     dict[article_sentences[i]] = results[i]
-     #dict[results[i]] = article_sentences[i]     # maybe it's better to switch keys and values
+     #dict[article_sentences[i]] = [results_article[i], sentiment_VADER[i], sentiment_Google[i]]
+     dict[repr(results_article[i])] = [article_sentences[i], sentiment_VADER[i]]#, sentiment_Google[i]]
      test['sentences'] = dict
 
 
