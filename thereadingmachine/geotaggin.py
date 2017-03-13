7ec3b7449d2d9ecff16431eb7c852091786a404a
marcogarieri <marcogarieri@gmail.com>
2017-03-13 09:13:25 +0100

    No stashing

--- thereadingmachine/geotaggin.py
+++ thereadingmachine/geotaggin.py
@@ -55,36 +55,8 @@ with open(input_file_name, 'rU') as f:
 d_countries = defaultdict(list)
 [make_dict_countries(l, d_countries) for l in list_countries]
 
-#located_keywords = set([word for word in keywords if word in text])
-
-tt = check_countries(text1, d_countries)
-tt1 = check_countries(text2, d_countries)
-
-
-a = [key for key in d_countries.keys() if any(words in text1 for words in d_countries[key])]
-
-t = 1
 
 
 
 
 
-#def readGene(f,g_repository,gname = "",  samples_idx = 0):
-#    try:
-#        genename = ""
-#        oldgene = ""
-#        for lraw in f:
-#            lraw = lraw.strip()
-#            l = lraw.split('\t') #######tab separated                                                                                                                      
-#            if len(l)<3:
-#              l = lraw.strip().split(',') #######tab separated                                                                                                             
-#            for i in range(0,samples_idx):
-#              l[i]=l[i].replace("-","|") ##bloody csv corrections for annotations                                                                                          
-#            genename = l[4].split('(')[0]
-#
-#            if '-' not in genename and (genename == gname or gname == ""):
-#                if len(l)>1:
-#                  g_repository[genename].append(l)
-#    except:
-#        raise StopIteration
-#    return g_repository
