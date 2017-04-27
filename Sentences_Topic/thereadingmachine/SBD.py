d030cc4b9ce98a5e47dbba59dc742307b4177b08
mk <mkao006@gmail.com>
2017-04-27 22:32:45 +0800

    Revert "Merge remote-tracking branch 'origin/Alberto' into mk"
    This reverts commit 531143324f89c6a78832f65bddf4a55cf598b1b0, reversing
    changes made to 2190732ab4c7ffb6a31d8b7b9e39b0afa4525493.

--- Sentences_Topic/thereadingmachine/SBD.py
+++
@@ -1,7 +0,0 @@
-from nltk.tokenize import sent_tokenize
-from thereadingmachine.textcleaner import textcleaner
-
-# Sentence Boundary Definition
-def SBD(test):
-    article_sentences = sent_tokenize(textcleaner(test['article']))
-    return article_sentences
