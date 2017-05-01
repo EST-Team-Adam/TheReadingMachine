164ef903429711768a8f3f6b2562829c48d4ba05
mk <mkao006@gmail.com>
2017-05-01 20:26:44 +0800

    added article dates

--- pipeline/data_harmonisation/controller.py
+++ pipeline/data_harmonisation/controller.py
@@ -1,4 +1,6 @@
+import os
 import pandas as pd
+from sqlalchemy import create_engine
 
 
 def read_data_inputs(source_data_table, engine, id_field):
@@ -7,6 +9,15 @@ def read_data_inputs(source_data_table, engine, id_field):
         sql_query = 'SELECT * FROM {}'.format(current_table)
         table_list.append(pd.read_sql(sql_query, engine))
 
+    table_list.append(get_raw_article_with_dates())
     merged_table = reduce(lambda x, y: pd.merge(x, y, on=id_field), table_list)
 
     return merged_table
+
+
+def get_raw_article_with_dates():
+    data_dir = os.environ['DATA_DIR']
+    engine = create_engine(
+        'sqlite:///{0}/the_reading_machine.db'.format(data_dir))
+    dated_article = pd.read_sql('SELECT id, date FROM RawArticle', engine)
+    return dated_article
