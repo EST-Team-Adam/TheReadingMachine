b54215afe9aed50305751f0ba04cbca3cfdc9802
albertomun <alberto.munisso@gmail.com>
2017-05-03 11:28:00 +0200

    sentence_selector update
    articles_sentiment and all_sentences_analyzer functions for whole corpus analysis

--- Sentiment Scoring/thereadingmachine/sentence_selector.py
+++ Sentiment Scoring/thereadingmachine/sentence_selector.py
@@ -7,6 +7,9 @@ from thereadingmachine.sentence_keywords import sentence_keywords
 from nltk.stem.snowball import SnowballStemmer
 from thereadingmachine.sentiment_VADER import VADER2
 from thereadingmachine.sentiment_GoogleNLP import GoogleNLP2
+from nltk.tokenize import sent_tokenize                    # for the all sentences analysis
+from thereadingmachine.textcleaner import textcleaner      # for the all sentences analysis
+import numpy as np    # for a unique sentiment score per each article
 
 
 def keyword_alarm(article_sentences, checkwords):               # sentence selector core script
@@ -58,7 +61,7 @@ def wordslist(words_list):
     return checkwords2
 
 
-def sentences_analyzer(tests, checkwords):  
+def sentences_analyzer(tests, checkwords):              # analyzes just selected sentences
   #checkwords = wordslist(checkwords)  
   analyzed_sentences = list()
   list_of_words = list()
@@ -70,12 +73,28 @@ def sentences_analyzer(tests, checkwords):
          dict['id'] = test['id']
          dict['sentences'] = keyword_alarm(article_sentences,checkwords)   # resulting dict is dict['commodity'][number(article)][sentence]         
          analyzed_sentences.append(dict)
-  return analyzed_sentences
+  return analyzed_sentences  
+
+
+def all_sentences_analyzer(tests):           # analyzes all the sentences
+  all_sentences = list()
+  sentiment_VADER = list()
+  #sentiment_Google = list()              # Activate this for Google NLP
+  for test in tests:
+      article_sentences = list()
+      dict = {'sentences':[], 'date':[], 'id':[]}
+      article_sentences = sent_tokenize(textcleaner(test['article']))
+      dict['date'] = test['date']
+      dict['id'] = test['id']
+      for sentence in article_sentences:
+          sentiment_VADER.append(VADER2(sentence))
+          #sentiment_Google.append(GoogleNLP2(sentence))              # Activate this for Google NLP
+      dict['sentences'] = zip(article_sentences, sentiment_VADER)#, sentiment_Google)  
+      all_sentences.append(dict)
+  return all_sentences
   
-  
-  
-  
-def sentences_analyzer_twitter(tests, checkwords):  
+
+def sentences_analyzer_twitter(tests, checkwords):    # selects and extracts sentences from twitter data
   #checkwords = wordslist(checkwords)  
   analyzed_sentences = list()
   list_of_words = list()
@@ -88,3 +107,34 @@ def sentences_analyzer_twitter(tests, checkwords):
          dict['sentences'] = keyword_alarm(article_sentences,checkwords)   # resulting dict is dict['commodity'][number(article)][sentence]         
          analyzed_sentences.append(dict)
   return analyzed_sentences
+
+
+def articles_sentiment(analyzed_sentences):      # computes a unique sentiment score per each article using the mean
+    articles_sentiment_scores = list()
+    for item in analyzed_sentences:
+        dic = {'date':[], 'id':[], 'positive_sentiment':[], 'neutral_sentiment':[], 'negative_sentiment':[], 'compound_sentiment':[]}
+        positive_sentiment = list()
+        neutral_sentiment = list()
+        negative_sentiment = list()
+        compound_sentiment = list()
+        article_pos_sent = list()
+        article_neu_sent = list()
+        article_neg_sent = list()
+        article_compound_sent = list()
+        for sentence in item['sentences']:
+            positive_sentiment.append(sentence[1]['pos'])
+            neutral_sentiment.append(sentence[1]['neu'])
+            negative_sentiment.append(sentence[1]['neg'])
+            compound_sentiment.append(sentence[1]['compound'])    # these lists are just for the last one
+        article_pos_sent = np.mean(positive_sentiment)
+        article_neu_sent = np.mean(neutral_sentiment)
+        article_neg_sent = np.mean(negative_sentiment)
+        article_compound_sent = np.mean(compound_sentiment)
+        dic['date'] = item['date']
+        dic['id'] = item['id']
+        dic['positive_sentiment'] = article_pos_sent
+        dic['neutral_sentiment'] = article_neu_sent
+        dic['negative_sentiment'] = article_neg_sent
+        dic['compound_sentiment'] = article_compound_sent
+        articles_sentiment_scores.append(dic)
+    return articles_sentiment_scores
