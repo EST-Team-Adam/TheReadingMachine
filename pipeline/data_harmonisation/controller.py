2756855e15cc55492eb195a5b8c601617bab24b9
mk <mkao006@gmail.com>
2017-05-11 21:54:44 +0800

    completed the harmonisation

--- pipeline/data_harmonisation/controller.py
+++ pipeline/data_harmonisation/controller.py
@@ -3,21 +3,59 @@ import pandas as pd
 from sqlalchemy import create_engine
 
 
-def read_data_inputs(source_data_table, engine, id_field):
-    table_list = list()
-    for current_table in source_data_table:
-        sql_query = 'SELECT * FROM {}'.format(current_table)
-        table_list.append(pd.read_sql(sql_query, engine))
-
-    table_list.append(get_raw_article_with_dates())
-    merged_table = reduce(lambda x, y: pd.merge(x, y, on=id_field), table_list)
-
-    return merged_table
+# def read_data_inputs(source_data_table, engine, id_field):
+#     table_list = list()
+#     for current_table in source_data_table:
+#         sql_query = 'SELECT * FROM {}'.format(current_table)
+#         table_list.append(pd.read_sql(sql_query, engine))
+#     table_list.append(get_raw_article_with_dates())
+#     merged_table = reduce(lambda x, y: pd.merge(x, y, on=id_field), table_list)
+#     return merged_table
 
 
-def get_raw_article_with_dates():
+# def get_raw_article_with_dates():
+#     data_dir = os.environ['DATA_DIR']
+#     engine = create_engine(
+#         'sqlite:///{0}/the_reading_machine.db'.format(data_dir))
+#     dated_article = pd.read_sql('SELECT id, date FROM RawArticle', engine)
+#     return dated_article
+
+
+def get_sentiment_scored_article():
     data_dir = os.environ['DATA_DIR']
     engine = create_engine(
         'sqlite:///{0}/the_reading_machine.db'.format(data_dir))
-    dated_article = pd.read_sql('SELECT id, date FROM RawArticle', engine)
-    return dated_article
+    sentiment_scored_article = pd.read_sql(
+        'SELECT * FROM SentimentScoredArticle', engine)
+    return sentiment_scored_article
+
+
+def get_topic_modelled_article():
+    data_dir = os.environ['DATA_DIR']
+    engine = create_engine(
+        'sqlite:///{0}/the_reading_machine.db'.format(data_dir))
+    topic_modelled_article = pd.read_sql('SELECT * FROM TopicModel', engine)
+    return topic_modelled_article
+
+
+def get_commodity_tagged_article():
+    data_dir = os.environ['DATA_DIR']
+    engine = create_engine(
+        'sqlite:///{0}/the_reading_machine.db'.format(data_dir))
+    commodity_tagged_article = pd.read_sql(
+        'SELECT * FROM CommodityTaggedArticle', engine)
+    return commodity_tagged_article
+
+
+def harmonise_article():
+
+    sentiment_scored_article = get_sentiment_scored_article()
+    topic_modelled_article = get_topic_modelled_article()
+    commodity_tagged_article = get_commodity_tagged_article()
+
+    processed_article_list = [sentiment_scored_article, topic_modelled_article,
+                              commodity_tagged_article]
+
+    harmonised_article = reduce(lambda dfx, dfy: pd.merge(dfx, dfy, on='id', how='inner'),
+                                processed_article_list)
+    return harmonised_article
--- pipeline/data_harmonisation/processor.py
+++ pipeline/data_harmonisation/processor.py
@@ -8,10 +8,9 @@ source_data_table = ['TopicModel', 'CommodityTaggedArticle']
 target_data_table = 'HarmonisedData'
 engine = create_engine('sqlite:///{0}/the_reading_machine.db'.format(data_dir))
 
-# Read and merge all data sources
-merged_data = ctr.read_data_inputs(
-    source_data_table=source_data_table, engine=engine, id_field='id')
+# Harmonise data
+harmonised_article = ctr.harmonise_article()
 
 # Save back to database
-merged_data.to_sql(con=engine, name=target_data_table, index=False,
-                   if_exists='replace')
+harmonised_article.to_sql(con=engine, name=target_data_table, index=False,
+                          if_exists='replace')
