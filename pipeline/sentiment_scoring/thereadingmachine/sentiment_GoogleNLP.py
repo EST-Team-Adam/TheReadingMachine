eb1a5197cc8a986c609a0529563b6c0e9fc4654a
mk <mkao006@gmail.com>
2017-06-08 19:23:37 +0800

    Revert "Update sentiment_GoogleNLP.py"
    This reverts commit 3634b13097e73be88425edb7ff9b3fa768418d9f.

--- pipeline/sentiment_scoring/thereadingmachine/sentiment_GoogleNLP.py
+++ pipeline/sentiment_scoring/thereadingmachine/sentiment_GoogleNLP.py
@@ -2,43 +2,33 @@ import httplib2                         # Google NLP Authetication
 from googleapiclient import discovery
 from oauth2client.client import GoogleCredentials
 from json import dump
-import time
-from retrying import retry
 
 
-@retry#(stop_max_attempt_number=10, wait_random_min=1000, wait_random_max=10000)
 def retrieve_sentiment(string):
     credentials = GoogleCredentials.get_application_default()
     service = discovery.build('language', 'v1beta1', credentials=credentials)
     service_request = service.documents().analyzeSentiment(
         body={
-             'document': {
-                         'type': 'PLAIN_TEXT',
-                         'content': string,
-                         }
-             }
-        )
-    try:
-        response = service_request.execute()
-        polarity = response['documentSentiment']['polarity']
-        magnitude = response['documentSentiment']['magnitude']
-        return {'polarity': polarity, 'magnitude': magnitude}
-    except KeyboardInterrupt:
-        raise
-    except:
-        print 'Block of text not accepted by Google NLP'
-
-
+            'document': {
+                'type': 'PLAIN_TEXT',
+                'content': string,
+            }
+        }
+    )
+    response = service_request.execute()
+    polarity = response['documentSentiment']['polarity']
+    magnitude = response['documentSentiment']['magnitude']
+    return {'polarity': polarity, 'magnitude': magnitude}
+    
 
 # Google NLP
 def GoogleNLP(article_sentences):
     sentiment_Google = list()
     for sentence in article_sentences:
-        sent = retrieve_sentiment(sentence)
-        sentiment_Google.append(sent)  
+       sentiment_Google.append(retrieve_sentiment(sentence))
     return sentiment_Google
     
 
-def GoogleNLP2(sentence):                       
+def GoogleNLP2(sentence):
     sentiment_Google = retrieve_sentiment(sentence)
     return sentiment_Google
