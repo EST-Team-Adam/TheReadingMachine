93a4821d004ff38b94244cf16a3ca40089837fe3
mk <mkao006@gmail.com>
2017-08-03 16:51:03 +0800

    added text processing

--- pipeline/article_processing/controller.py
+++ pipeline/article_processing/controller.py
@@ -1,4 +1,9 @@
+from __future__ import division
 import os
+import itertools
+import pandas as pd
+from nltk.corpus import stopwords
+from nltk.tokenize import RegexpTokenizer
 from datetime import datetime
 
 # Load and set some manipulation parameters
@@ -20,7 +25,14 @@ irrelevant_link = ['https://www.euractiv.com/topics/news/?type_filter=video',
                    'http://www.euractiv.com/news/']
 
 
-def process_articles(raw_articles):
+# Initialise processing parameters
+remove_captalisation = True
+remove_noun = True
+remove_numerical = True
+stem = False
+
+
+def scraper_post_processing(raw_articles):
 
     # Remove the original id and drop duplciates
     processed_articles = (raw_articles
@@ -44,3 +56,95 @@ def process_articles(raw_articles):
     processed_articles['id'] = range(1, processed_articles.shape[0] + 1)
 
     return processed_articles
+
+
+def text_processor(text, remove_captalisation=True, remove_noun=True,
+                   remove_numerical=True, stem=False):
+    '''The function process the texts with the intention for topic
+    modelling.
+
+    The following steps are performed:
+    1. Tokenise
+    2. Prune words
+    3. Removal of stopwords
+
+    Details:
+
+    The regular expression tokeniser is used as we are interested just
+    on the key words, punctuation is irrelevant.  There are two
+    options for word pruning, either stemming or lemmatisation.  The
+    standard snowball stemmer is currently implemented, however, for
+    topic modelling we should consider lemmatisation over stemming.
+
+    '''
+    # TODO (Michael): Split words by '_', and maybe remove punctuations first.
+
+    # Tokenize
+    tokenizer = RegexpTokenizer(r'\w+')
+    tokenized_text = tokenizer.tokenize(text)
+
+    # # Prune
+    # if lemmatization:
+    #     lemmatizer = WordNetLemmatizer()
+    #     pruned_text = [lemmatizer.lemmatize(word)
+    #                    for word in tokenized_text]
+    # else:
+    #     stemmer = SnowballStemmer("english")
+    #     pruned_text = [stemmer.stem(word) for word in tokenized_text]
+
+    # This option is available as certain capital word has intrinsic
+    # meaning. e.g. Apple vs apple.
+    if remove_captalisation:
+        tokenized_text = [word.lower() for word in tokenized_text]
+
+    if remove_numerical:
+        tokenized_text = [word for word in tokenized_text
+                          if not word.isdigit()]
+
+    # Remove stopwords and manual exlusion set
+    meaningless_words = ['euractiv', 'com',
+                         'bloomberg', 'reuters', 'jpg', 'png']
+    exclusion_words = stopwords.words('english') + meaningless_words
+
+    nonstopword_text = [word.lower()
+                        for word in tokenized_text
+                        if word.lower() not in exclusion_words]
+    return nonstopword_text
+
+
+def article_summariser(article_list):
+
+    article_count = len(article_list)
+    vocab_size = len(set(itertools.chain.from_iterable(article_list)))
+    article_length = [len(t) for t in article_list]
+    article_vocab_size = [len(set(t)) for t in article_list]
+    lexical_diversity = [vs / l if l != 0 else 0
+                         for l, vs in zip(article_length, article_vocab_size)]
+
+    average_lexical_diversity = sum(lexical_diversity) / len(lexical_diversity)
+    average_article_length = sum(article_length) / len(article_length)
+    summary = {'createTime': datetime.utcnow(),
+               'article_count': article_count,
+               'vocab_size': vocab_size,
+               'average_lexical_diversity': average_lexical_diversity,
+               'average_article_length': average_article_length}
+    return pd.DataFrame(summary, index=[0])
+
+
+def text_preprocessing(article_df, article_col, min_length):
+    tokenised_text = [text_processor(a,
+                                     remove_captalisation=remove_captalisation,
+                                     remove_noun=remove_noun,
+                                     remove_numerical=remove_numerical,
+                                     stem=stem)
+                      for a in article_df[article_col]]
+
+    min_length_ind = [i for i, t in enumerate(tokenised_text)
+                      if len(t) > min_length]
+    min_length_tokens = [tokenised_text[i] for i in min_length_ind]
+    exclude_min_length_df = article_df.iloc[min_length_ind, ].copy()
+
+    summary = article_summariser(min_length_tokens)
+    exclude_min_length_df[article_col] = [' '.join(tt)
+                                          for tt in min_length_tokens]
+    return exclude_min_length_df, summary
--- pipeline/article_processing/processor.py
+++ pipeline/article_processing/processor.py
@@ -6,32 +6,53 @@ from sqlalchemy import create_engine
 
 
 # Configuration
+min_length = 30
 data_dir = os.environ['DATA_DIR']
 source_data_table = 'RawArticle'
 target_data_table = 'ProcessedArticle'
+summary_data_table = 'ProcessedArticleSummary'
 engine = create_engine('sqlite:///{0}/the_reading_machine.db'.format(data_dir))
 sql_query = 'SELECT * FROM {}'.format(source_data_table)
 
 # Reading data
 articles = pd.read_sql(sql_query, engine, parse_dates=['date'])
 
-# Process the articles
-processed_articles = ctr.process_articles(articles)
+# Process the articles.
+#
+# We first post process the result of the scraper by removeing
+# duplicates and several other data issues. Then we process the text
+# by tokenise and removal of unwanted symbols.
+processed_articles = ctr.scraper_post_processing(articles)
+preprocessed_text, text_summary = ctr.text_preprocessing(article_df=processed_articles,
+                                                         article_col='article',
+                                                         min_length=min_length)
 
 
-# Save back to database
-field_type = {'id': sqlalchemy.types.Integer(),
-              'date': sqlalchemy.types.Date(),
-              'article': sqlalchemy.types.Text(),
-              'title': sqlalchemy.types.NVARCHAR(300),
-              'source': sqlalchemy.types.NVARCHAR(20),
-              'link': sqlalchemy.types.NVARCHAR(255)
-              }
+data_field_type = {'id': sqlalchemy.types.Integer(),
+                   'date': sqlalchemy.types.Date(),
+                   'article': sqlalchemy.types.Text(),
+                   'title': sqlalchemy.types.NVARCHAR(300),
+                   'source': sqlalchemy.types.NVARCHAR(20),
+                   'link': sqlalchemy.types.NVARCHAR(255)
+                   }
+
+summary_field_type = {'createTime': sqlalchemy.types.DateTime(),
+                      'article_count': sqlalchemy.types.Integer(),
+                      'average_article_length': sqlalchemy.types.Float(),
+                      'average_lexical_diversity': sqlalchemy.types.Float(),
+                      'vocab_size': sqlalchemy.types.Integer()
+                      }
 
 
 # Save the data
-processed_articles.to_sql(con=engine,
-                          name=target_data_table,
-                          index=False,
-                          if_exists='replace',
-                          dtype=field_type)
+preprocessed_text.to_sql(con=engine,
+                         name=target_data_table,
+                         index=False,
+                         if_exists='replace',
+                         dtype=data_field_type)
+
+text_summary.to_sql(con=engine,
+                    name=summary_data_table,
+                    index=False,
+                    if_exists='append',
+                    dtype=summary_field_type)
