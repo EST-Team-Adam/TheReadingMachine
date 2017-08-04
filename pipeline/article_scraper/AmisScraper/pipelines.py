9c01fd0697784710245ebe5b1971d5adebbfe716
mrpozzi <poz.luc@gmail.com>
2017-08-04 09:21:50 -0700

    Fixed the truncation issue and the Unicode handling

--- pipeline/article_scraper/AmisScraper/pipelines.py
+++ pipeline/article_scraper/AmisScraper/pipelines.py
@@ -21,7 +21,6 @@ class DuplicatesPipeline(object):
 
     def process_item(self, item, spider):
         if item['link'] in self.ids_seen:
-            # spider.logf.write("Duplicate item found: %s\n" % item)
             raise DropItem("Duplicate item found: %s" % item)
         else:
             self.ids_seen.add(item['link'])
@@ -42,33 +41,43 @@ class SanitizeArticlePipeline(object):
     def process_item(self, item, spider):
         if 'article' in dict(item):
             sanitized_article = " ".join(
-                [x for x in item['article'] if self._check_stop_words(x)])
-            sanitized_article = sanitized_article.replace(
-                '\n', '').replace('\t', ' ')
-            item['article'] = sanitized_article.encode('ascii', 'ignore')
+                    [x for x in item['article'] if self._check_stop_words(x)])
+            sanitized_article = sanitized_article.replace('\n', '')\
+            .replace('\t', ' ')
+            try:
+                sanitized_article = sanitized_article.encode('utf-8', 'ignore')
+            except UnicodeDecodeError:
+                sanitized_article = sanitized_article.decode('unicode_escape').encode('utf-8', 'ignore')
+            item['article'] = sanitized_article
         if len(item['date']) == 0:
-            # spider.logf.write("Empty Date in %s\n" % item)
             raise DropItem("Empty Date in %s" % item)
         elif len(item['article']) > 0:
             return item
         else:
-            # spider.logf.write("Empty Article in %s\n" % item)
             raise DropItem("Empty Article in %s" % item)
 
 
 class AmisJsonPipeline(object):
-    lock = threading.Lock()
-    datafile = open(data_dir + '/blog_articles_{0}.jsonl'.format(
-        time.strftime("%d_%m_%Y")), 'a')
 
     def __init__(self):
         pass
 
+    def open_spider(self, spider):
+        self.datafile = open(data_dir + '/blog_articles_{0}.jsonl'.format(
+            time.strftime("%d_%m_%Y")), 'a')
+        self.lock = threading.Lock()
+
     def process_item(self, item, spider):
-        item_dict = dict(item)
-        item_dict['source'] = spider.name
-        line = json.dumps(item_dict) + "\n"
-        AmisJsonPipeline.lock.acquire()
-        AmisJsonPipeline.datafile.write(line)
-        AmisJsonPipeline.lock.release()
+        self.lock.acquire()
+        try:
+            item_dict = dict(item)
+            item_dict['source'] = spider.name
+            line = json.dumps(item_dict, ensure_ascii=False) + "\n"
+            self.datafile.write(line)
+        except (UnicodeDecodeError, UnicodeEncodeError):
+            pass
+        self.lock.release()
         return item
+
+    def close_spider(self, spider):
+        self.datafile.close()
--- pipeline/article_scraper/AmisScraper/spiders/spiders.py
+++ pipeline/article_scraper/AmisScraper/spiders/spiders.py
@@ -2,6 +2,35 @@ from AmisScraper.items import NewsArticleItem
 from scrapy.linkextractors import LinkExtractor
 from scrapy.spiders import CrawlSpider, Rule
 from datetime import datetime
+from scrapy.selector import HtmlXPathSelector
+from scrapy.contrib.linkextractors.sgml import SgmlLinkExtractor
+from scrapy.utils.response import get_base_url
+
+
+
+class UnicodeFriendlyLinkExtractor(SgmlLinkExtractor):
+    """
+    Need this to fix the encoding error.
+    Taken from https://stackoverflow.com/questions/17862016/scrapy-python-unicode-links-error
+    """
+    def extract_links(self, response):
+        base_url = None
+        if self.restrict_xpaths:
+            hxs = HtmlXPathSelector(response)
+            base_url = get_base_url(response)
+            body = u''.join(f for x in self.restrict_xpaths
+                           for f in hxs.select(x).extract())
+            try:
+                body = body.encode(response.encoding)
+            except UnicodeEncodeError:
+                body = body.encode('utf-8')
+        else:
+            body = response.body
+
+        links = self._extract_links(body, response.url, response.encoding, base_url)
+        links = self._process_links(links)
+        return links
+
 
 
 class BloombergSpider(CrawlSpider):
@@ -10,11 +39,11 @@ class BloombergSpider(CrawlSpider):
     allowed_domains = ["bloomberg.com"]
     start_urls = ["http://www.bloomberg.com"]
     rules = [
-        Rule(LinkExtractor(allow='(/news/articles/)((?!:).)*$'),
+        Rule(UnicodeFriendlyLinkExtractor(allow='(/news/articles/)((?!:).)*$'),
              callback="parse_item", follow=True),
-        Rule(LinkExtractor(allow='(/news/)((?!:).)*$'),
+        Rule(UnicodeFriendlyLinkExtractor(allow='(/news/)((?!:).)*$'),
              callback="parse_item", follow=True),
-        Rule(LinkExtractor(allow='(/articles/)((?!:).)*$'),
+        Rule(UnicodeFriendlyLinkExtractor(allow='(/articles/)((?!:).)*$'),
              callback="parse_item", follow=True)
     ]
 
@@ -26,7 +55,7 @@ class BloombergSpider(CrawlSpider):
         try:
             item['title'] = title
             item['article'] = article
-            item['link'] = response.url
+            item['link'] = response.url.replace('http://', '').replace('https://', '')
             raw_date = response.url.split("/")[-2]
             date = datetime.strptime(raw_date, '%Y-%m-%d')
             item['date'] = str(date)
@@ -42,7 +71,7 @@ class NoggersBlogSpider(CrawlSpider):
     allowed_domains = ["nogger-noggersblog.blogspot.it"]
     start_urls = ["http://nogger-noggersblog.blogspot.it/"]
     rules = [
-        Rule(LinkExtractor(allow='((?!:).)*html$'),
+        Rule(UnicodeFriendlyLinkExtractor(allow='((?!:).)*html$'),
              callback="parse_item", follow=True)
     ]
 
@@ -53,7 +82,7 @@ class NoggersBlogSpider(CrawlSpider):
         self.logger.info("Scraping Title: " + title)
         item['title'] = title
         item['article'] = article
-        item['link'] = response.url
+        item['link'] = response.url.replace('http://', '').replace('https://', '')
         try:
             token = filter(lambda x: '--' in x, article)
             try:
@@ -80,11 +109,11 @@ class WorldGrainSpider(CrawlSpider):
                  ["http://www.world-grain.com/News/Archive.aspx?page={0}&year={1}&month=0".format(i, j)
                   for i in range(1, 50) for j in range(2006, 2017)]
     rules = [
-        Rule(LinkExtractor(allow='(/articles/news_home/)((?!:).)*$'),
+        Rule(UnicodeFriendlyLinkExtractor(allow='(/articles/news_home/)((?!:).)*$'),
              callback="parse_item", follow=True),
-        Rule(LinkExtractor(allow='(/news_home/)((?!:).)*$'),
+        Rule(UnicodeFriendlyLinkExtractor(allow='(/news_home/)((?!:).)*$'),
              callback="parse_item", follow=True),
-        Rule(LinkExtractor(allow='(/articles/)((?!:).)*$'),
+        Rule(UnicodeFriendlyLinkExtractor(allow='(/articles/)((?!:).)*$'),
              callback="parse_item", follow=True)
     ]
 
@@ -96,7 +125,8 @@ class WorldGrainSpider(CrawlSpider):
         self.logger.info("Scraping Title: " + title)
         item['title'] = title
         item['article'] = article
-        item['link'] = response.url
+        item['link'] = response.url.replace('http://', '').replace('https://', '')
+
         try:
             raw_date = "{0}-{1}".format(response.url.split("/")
                                         [-2], response.url.split("/")[-3])
@@ -114,20 +144,24 @@ class EuractivSpider(CrawlSpider):
     allowed_domains = ["www.euractiv.com"]
     start_urls = ["http://www.euractiv.com"]
     rules = [
-        Rule(LinkExtractor(allow='(/agriculture-food/news/)((?!:).)*$'),
+        Rule(UnicodeFriendlyLinkExtractor(allow='(/agriculture-food/news/)((?!:).)*$'),
              callback="parse_item", follow=True),
-        Rule(LinkExtractor(allow='(/news/)((?!:).)*$'),
+        Rule(UnicodeFriendlyLinkExtractor(allow='(/news/)((?!:).)*$'),
              callback="parse_item", follow=True)
     ]
 
     def parse_item(self, response):
         item = NewsArticleItem()
-        title = response.xpath('//title/text()')[0].extract()
-        article = response.xpath('//p/text()').extract()
+        try:
+            title = response.xpath('//title/text()')[0].extract()
+            article = response.xpath('//p/text()').extract()
+            item['title'] = title
+            item['article'] = article
+            item['link'] = response.url.replace('http://', '').replace('https://', '')
+        except UnicodeDecodeError:
+                pass
         self.logger.info("Scraping Title: " + title)
-        item['title'] = title
-        item['article'] = article
-        item['link'] = response.url
+        
         try:
             raw_date = response.xpath(
                 '//*[contains(@class,"ea-dateformat")]/text()').extract()[1].strip()
@@ -147,36 +181,41 @@ class AgriMoneySpider(CrawlSpider):
         "http://www.agrimoney.com",
         "http://www.agrimoney.com/search/",
         "http://www.agrimoney.com/searchdate/"
-    ] + ["http://www.agrimoney.com/search/{0}/".format(i) for i in range(500)] + \
+    ] + ["http://www.agrimoney.com/search/{0}/".format(i) for i in range(600)] + \
         ["http://www.agrimoney.com/searchdate/{0}/".format(i)
          for i in range(500)]
     rules = [
-        Rule(LinkExtractor(allow='(/news/)((?!:).)*html$'),
+        Rule(UnicodeFriendlyLinkExtractor(allow='(/news/)((?!:).)*html$'),
              callback="parse_item", follow=True),
-        Rule(LinkExtractor(allow='(/feature/)((?!:).)*html$'),
+        Rule(UnicodeFriendlyLinkExtractor(allow='(/feature/)((?!:).)*html$'),
              callback="parse_item", follow=True),
-        Rule(LinkExtractor(allow='(/marketreport/)((?!:).)*html$'),
+        Rule(UnicodeFriendlyLinkExtractor(allow='(/marketreport/)((?!:).)*html$'),
              callback="parse_item", follow=True)
     ]
 
     def parse_item(self, response):
+        cleaned_response = response.replace(
+            body=response.body_as_unicode().encode('utf-8', 'ignore'),
+            encoding='utf-8'
+            )
         item = NewsArticleItem()
-        title = response.xpath('//title/text()')[0].extract()
-        raw_date = response.xpath('//font/text()')[2].extract().split(',')[1] + \
-            response.xpath('//font/text()')[3].extract().split(',')[0]
-        article = response.xpath('//body//text()').extract()
-        self.logger.info("Scraping Title: " + title)
-        item['title'] = title.replace('Agrimoney.com | ', '')
-        item['article'] = article
-        item['link'] = response.url
         try:
-            date = datetime.strptime(raw_date.split(
-                ',')[1].replace('Sept', 'Sep'), ' %d %b %Y')
+            title = response.xpath('//title/text()')[0].extract().encode('utf-8','ignore')
+            raw_date = response.xpath('//font/text()')[2].extract().split(',')[1] + \
+            response.xpath('//font/text()')[3].extract().split(',')[0]
+            self.logger.info("Scraping Title: " + title)
+            item['title'] = title.replace('Agrimoney.com | ', '').encode('utf-8','ignore')
+            item['article'] = [art.encode('utf-8') for art in response.xpath('//body//text()').extract()]
+            item['link'] = response.url.replace('http://', '').replace('https://', '').encode('utf-8','ignore')
+        except UnicodeDecodeError:
+            pass
+        try:
+            date = datetime.strptime(raw_date.split(',')[1].replace('Sept', 'Sep'), ' %d %b %Y')
             item['date'] = str(date)
             return item
         except (ValueError, IndexError):
-            raw_date = response.xpath(
-                '//font/text()')[2].extract() + response.xpath('//font/text()')[3].extract()
+            raw_date = response.xpath('//font/text()')[2].extract() + \
+                response.xpath('//font/text()')[3].extract()
             try:
                 date = datetime.strptime(raw_date.split(',')[2], ' %d %b %Y')
                 item['date'] = str(date)
